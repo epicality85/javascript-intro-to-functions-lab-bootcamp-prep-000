77733916f1e323fd67846b668d88b50cf88fda7b
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-05 12:33:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // learning to use functions that have specific outcomes
 
+// function to return upper case version of overload
 function shout(string)
 {
   return string.toUpperCase() // simplify to one call, not: string = string.blah(), return string
 }
 
+// function to return lower case version of overload
 function whisper(string)
 {
   return string.toLowerCase()
 }
 
+// 
+function logShout(string)
+{
+  
+}
