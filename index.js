e805fa70a5f0dec1198b4cc4e09fb2f075bfbd27
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-05 12:30:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// learning to use functions that have specific outcomes
+
 function shout(string)
 {
   return string.toUpperCase() // simplify to one call, not: string = string.blah(), return string
 }
+
