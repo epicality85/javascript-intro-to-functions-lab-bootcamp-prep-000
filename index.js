28f49be1ea6858eec51833a8c5e5a2a79fb8e8e7
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-05 12:31:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function shout(string)
   return string.toUpperCase() // simplify to one call, not: string = string.blah(), return string
 }
 
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
